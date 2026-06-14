d569f9a7685d850fc1b814d115dce4488fe98b30
sm8192 <sm8192@gmail.com>
2026-06-14 09:37:28 -0500

    useeffect dependency

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -2,24 +2,24 @@
 
 import { useState, useEffect } from "react";
 
-export default function Landing () {
+export default function Landing() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('/api/room')
-        .then((res) => res.json())
-        .then((data) =>  {
-            setData(data);
-            setLoading(false);
-        })
-    })
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+    }, [])
 
     return (
         <div>
-            { loading ?
-            <h5>Loading...</h5> :
-            <p>{JSON.stringify(data)}</p>
+            {loading ?
+                <h5>Loading...</h5> :
+                <p>{JSON.stringify(data)}</p>
             }
         </div>
     )
