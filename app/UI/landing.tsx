87bb677c06b0b5a6d8d06853f9154f51b29a3b34
sm8192 <sm8192@gmail.com>
2026-08-02 18:17:22 -0500

    setting up form

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -1,30 +1,48 @@
 'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, SubmitEvent } from "react";
 
 export default function Landing() {
     const [data, setData] = useState(null);
+    const [roomCode, setRoomCode] = useState('');
     const [loading, setLoading] = useState(true);
 
-   /* useEffect(() => {
-        fetch('/api/room')
+    useEffect(() => {
+        console.log("Hey Shane, effect ran");
+
+        /*fetch('/api/room')
             .then((res) => res.json())
             .then((data) => {
                 setData(data);
                 setLoading(false);
-            })
-    }, []) */
+            }) */
+    }, [roomCode])
 
-    const roomCodeButtonClick = () => {
-        console.log("Hey Shane");
+    const roomCodeSubmit = (event: SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const newRoomCode = String(formData.get("room-code"));
+
+        setRoomCode(newRoomCode);
     }
 
 
     return (
         <div className="m-8 flex flex-col items-center">
-            <h5>Enter room code:</h5>
-            <div><input id='room-code-input' type="text" className="outline outline-slate-200 m-2 rounded-xs"></input></div>
-            <div><button className="outline-2 outline-slate-200 rounded-xl p-2 m-2" onClick={roomCodeButtonClick}>Enter Room</button></div>
+            <form onSubmit={roomCodeSubmit}>
+                <h5>Enter room code:</h5>
+                <div>
+                    <input id='room-code-input'
+                        name="room-code"
+                        type="text"
+                        className="outline outline-slate-200 m-2 rounded-xs text-center"></input>
+                </div>
+                <div>
+                    <button className="outline-2 outline-slate-200 rounded-xl p-2 m-2"
+                        type='submit'>Enter Room</button>
+                </div>
+            </form>
         </div>
     )
 }
