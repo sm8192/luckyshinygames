2919447dc8cd0c21437322bce4538f6eb969c8c5
sm8192 <sm8192@gmail.com>
2026-08-02 17:42:53 -0500

    padding and margins

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -23,8 +23,8 @@ export default function Landing() {
     return (
         <div className="m-8 flex flex-col items-center">
             <h5>Enter room code:</h5>
-            <div><input id='room-code-input' type="text" className="outline outline-slate-800"></input></div>
-            <div><button className="outline-2 outline-slate-800 rounded-xl p-2" onClick={roomCodeButtonClick}>Enter Room</button></div>
+            <div><input id='room-code-input' type="text" className="outline outline-slate-400" m-2></input></div>
+            <div><button className="outline-2 outline-slate-400 rounded-xl p-2 m-2" onClick={roomCodeButtonClick}>Enter Room</button></div>
             {loading ?
                 <h5>Loading...</h5> :
                 <p>Not Loading</p>
