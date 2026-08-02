5726e2d86dd8c85fa7c037b46f9f010e3137e1e5
sm8192 <sm8192@gmail.com>
2026-08-02 17:48:20 -0500

    More more css

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -16,19 +16,15 @@ export default function Landing() {
     }, []) */
 
     const roomCodeButtonClick = () => {
-
+        console.log("Hey Shane");
     }
 
 
     return (
         <div className="m-8 flex flex-col items-center">
             <h5>Enter room code:</h5>
-            <div><input id='room-code-input' type="text" className="outline outline-slate-400" m-2></input></div>
-            <div><button className="outline-2 outline-slate-400 rounded-xl p-2 m-2" onClick={roomCodeButtonClick}>Enter Room</button></div>
-            {loading ?
-                <h5>Loading...</h5> :
-                <p>Not Loading</p>
-            }
+            <div><input id='room-code-input' type="text" className="outline outline-slate-200 m-2 rounded-xs"></input></div>
+            <div><button className="outline-2 outline-slate-200 rounded-xl p-2 m-2" onClick={roomCodeButtonClick}>Enter Room</button></div>
         </div>
     )
 }
