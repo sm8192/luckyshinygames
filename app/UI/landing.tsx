098cc4be8c62702d9d9933fe75b23243f9081faf
sm8192 <sm8192@gmail.com>
2026-08-09 11:47:30 -0500

    data log

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -14,6 +14,7 @@ export default function Landing() {
             fetch('/api/room?room_id=' + code)
                 .then((res) => res.json())
                 .then((data) => {
+                    console.log("data is " + JSON.stringify(data));
                     setGame(data.game);
                     setBoardId(data.board_id);
                 })
