9330d726735ba11c0b60b8df48a5e8c47324a9a8
sm8192 <sm8192@gmail.com>
2026-08-09 12:22:08 -0500

    debugging sql

--- app/api/tic-tac-toe/route.tsx
+++ app/api/tic-tac-toe/route.tsx
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
         const searchParams = request.nextUrl.searchParams;
         const ticTacToeId = searchParams.get('id');
 
-        const response = await sql`SELECT board_state, players, active_player FROM tic-tac-toe_boards WHERE id = ${ticTacToeId}`;
+        const response = await sql`SELECT board_state, players, active_player FROM tic-tac-toe_boards WHERE id = 1`;
 
         return NextResponse.json(response, { status: 200 })
 
