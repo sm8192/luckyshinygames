cc9524e5562b19d5be3b9e1b6ab3683801d6d87d
sm8192 <sm8192@gmail.com>
2026-08-15 13:18:14 -0500

    breaking out room entry form

--- app/UI/landing.tsx
+++ app/UI/landing.tsx
@@ -3,6 +3,7 @@
 import { useState, SubmitEvent } from "react";
 import Room from "./room";
 import GameSelect from "./game_select";
+import RoomEntryForm from "./room_entry_form";
 
 export default function Landing() {
     const [game, setGame] = useState<string>('');
@@ -166,29 +167,7 @@ export default function Landing() {
     return (
         <div className="m-8">
             {!roomCode ?
-                <form className="flex flex-col items-center" onSubmit={roomCodeSubmit}>
-                    <h5>Enter room code:</h5>
-                    <div>
-                        <input id='room-code-input'
-                            name="room-code"
-                            type="text"
-                            className="outline outline-slate-200 m-2 rounded-xs text-center"></input>
-                    </div>
-                    {roomNotFound ?
-                        <h4 className="text-red-500">Room not found</h4> :
-                        <div className="hidden"></div>}
-                    <div>
-                        <button className="outline-2 outline-slate-200 rounded-xl p-2 m-2"
-                            type='submit'>Enter Room</button>
-                    </div>
-                    <div>
-                        <button className="outline-2 outline-slate-200 rounded-xl p-2 m-2"
-                            type='button'
-                            onClick={createNewRoom}>
-                            Create New Room
-                        </button>
-                    </div>
-                </form> :
+                <RoomEntryForm roomCodeSubmit={roomCodeSubmit} roomNotFound={roomNotFound} createNewRoom={createNewRoom} /> :
                 <div>
                     {!game ?
                         <GameSelect chooseGame={chooseGame} /> :
