952b0abd60f24d726d8bc62e62f4568632f2f1df
sm8192 <sm8192@gmail.com>
2026-08-22 10:15:12 -0500

    stringify, you idiot.

--- app/UI/Tic Tac Toe/tic-tac-toe_board.tsx
+++ app/UI/Tic Tac Toe/tic-tac-toe_board.tsx
@@ -22,7 +22,7 @@ export default function TicTacToeBoard({ boardId }: ticTacToeBoardProps) {
             fetch('/api/tic-tac-toe?id=' + id)
                 .then((res) => res.json())
                 .then((data) => {
-                    console.log("Hey Shane, data is " + data);
+                    console.log("Hey Shane, data is " + JSON.stringify(data));
                     setLoading(false);
                     setBoardState(data[0].board_state);
                     setPlayers(data[0].players);
